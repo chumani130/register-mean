57057c7fbec7811ab1d612031cbc901c1671ae1a
Chumani Madikizela <chumanimadikizela@gmail.com>
2023-12-01 11:00:13 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomrComponent } from './pages/homr/homr.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomrComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
