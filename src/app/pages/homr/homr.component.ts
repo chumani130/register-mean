2973cb2951583f9ab0e969e4adf2593ff8263cd4
Chumani Madikizela <chumanimadikizela@gmail.com>
2023-12-01 10:59:15 +0200

    Create homr.component.ts

---
+++ src/app/pages/homr/homr.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homr',
+  templateUrl: './homr.component.html',
+  styleUrls: ['./homr.component.scss']
+})
+export class HomrComponent {
+
+}
